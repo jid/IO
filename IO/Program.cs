152b91f9274c76086c09cfe0b2fba2c02db00180
jid <idczak.jaroslaw@gmail.com>
2015-07-06 21:01:43 +0200

    commit

--- IO/Program.cs
+++ IO/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IO
 {
@@ -10,7 +11,34 @@ namespace IO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test programu 2");
+            // Gathering information about HDDs
+            DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in drivesInfo)
+            {
+                Console.WriteLine("Drive {0}", driveInfo.Name);
+                Console.WriteLine("  File type: {0}", driveInfo.DriveType);
+                if (driveInfo.IsReady == true)
+                {
+                    Console.WriteLine("  Volume label: {0}", driveInfo.VolumeLabel);
+                    Console.WriteLine("  File system: {0}", driveInfo.DriveFormat);
+                    Console.WriteLine(
+                        "  Available space to current user:{0,15} bytes",
+                        driveInfo.AvailableFreeSpace);
+                    Console.WriteLine(
+                        "  Total available space:          {0,15} bytes",
+                        driveInfo.TotalFreeSpace);
+                    Console.WriteLine(
+                        "  Total size of drive:            {0,15} bytes ",
+                        driveInfo.TotalSize);
+                }
+            }
+
+            // Folder manipulations
+            var directory = Directory.CreateDirectory(@"C:\Temp\ProgrammingInCSharp\Directory");
+            var directoryInfo = new DirectoryInfo(@"C:\Temp\ProgrammingInCSharp\DirectoryInfo");
+            directoryInfo.Create();
+
+            // Do not close console window
             Console.ReadKey();
         }
     }
